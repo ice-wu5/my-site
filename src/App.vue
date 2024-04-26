e4b3d1e903267103b4b2ecf6780c3b17571f2c54
ice_wu <xuhong267@gmail.com>
2024-04-26 21:15:03 +0800

    add Empty Component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    app组件
+    <div class="app-container">
+      <Empty></Empty>
+    </div>
     <Avatar :size="200" src="https://cdn.pixabay.com/photo/2017/03/12/11/30/alishan-2136879_1280.jpg"></Avatar>
     <Icon type="icon-telephone"></Icon>
     <Pager @pageChange="pageChange" :current="current" :total="3" :pageNumber="10" :visibleNum="5"></Pager>
@@ -10,11 +12,13 @@
 import Avatar from '@/components/Avatar'
 import Icon from '@/components/Icon'
 import Pager from '@/components/Pager'
+import Empty from '@/components/Empty'
 export default ({
   components: {
     Avatar,
     Icon,
-    Pager
+    Pager,
+    Empty
   },
   data () {
     return {
@@ -32,6 +36,11 @@ export default ({
 </script>
 
 <style lang="less">
+.app-container{
+  width: 500px;
+  height: 400px;
+  background-color: #e8b4b4;
+}
 .icon-container{
   font-size: 30px;
   color: aquamarine;
---
+++ src/components/Empty/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="empty-container">
+    <img class="empty-img" src="https://img01.yzcdn.cn/vant/empty-image-default.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyIndex'
+}
+</script>
+
+<style lang="less" scoped>
+.empty-container{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .empty-img{
+    width: 50%;
+    height: 50%;
+    object-fit: cover;
+  }
+}
+</style>>
