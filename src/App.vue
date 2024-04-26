888be663817db60b45ac3fe95e9a9343ba782cc4
ice_wu <xuhong267@gmail.com>
2024-04-26 22:31:25 +0800

    add ImageLoader Component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div class="imageloader-container">
+      <ImageLoader @load="handleLoad" :src="'http://www.daxiaamu.com/wp-content/uploads/2014/11/4_ingoscholtes_uk.jpg'"  :duration="duration"></ImageLoader>
+    </div>
     <div class="app-container">
       <Empty></Empty>
     </div>
@@ -13,22 +16,28 @@ import Avatar from '@/components/Avatar'
 import Icon from '@/components/Icon'
 import Pager from '@/components/Pager'
 import Empty from '@/components/Empty'
+import ImageLoader from '@/components/ImageLoader'
 export default ({
   components: {
     Avatar,
     Icon,
     Pager,
-    Empty
+    Empty,
+    ImageLoader
   },
   data () {
     return {
-      current: 6
+      current: 6,
+      duration: 5
     }
   },
   methods: {
     pageChange (num) {
       console.log(num)
       this.current = num
+    },
+    handleLoad (num) {
+      console.log('原始图片加载完成了', num)
     }
   }
 
@@ -36,6 +45,10 @@ export default ({
 </script>
 
 <style lang="less">
+.imageloader-container{
+  width: 300px;
+  height: 200px;
+}
 .app-container{
   width: 500px;
   height: 400px;
---
+++ src/components/ImageLoader/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="imageloader-container">
+    <!-- 占位图片 -->
+    <img v-if="isLoaded===false" class="img-placeholder" :src="placeholder" alt="">
+    <!-- 实际图片 -->
+    <img @load="handleLoad"  class="img-origin" :class="{isShow:isShow,isHidden:!isShow}" :src="src" alt="">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageLoader',
+  data () {
+    return {
+      isLoaded: false,
+      isShow: false
+    }
+  },
+  props: {
+    duration: {
+      type: Number,
+      default: 6
+    },
+    src: {
+      type: String,
+      required: true
+    },
+    placeholder: {
+      type: String,
+      default: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_Y6mbPz3a0VRhzK9f0-xR7hUAULduVppnC5jLKsmWuQ&s'
+    }
+  },
+  methods: {
+    handleLoad (e) {
+      this.$emit('load', e)
+      setTimeout(() => {
+        this.isLoaded = true
+        this.isShow = true
+      }, this.duration * 1000)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.imageloader-container{
+  position: relative;
+  width: 100%;
+  height: 100%;
+  img{
+    position: absolute;
+    left: 0;
+    top:0;
+    width: 100%;
+    height: 100%;
+  }
+}
+.isShow{
+  opacity: 1;
+}
+.isHidden{
+  opacity: 0;
+}
+.img-origin{
+  z-index:1;
+  transition:0.5s;
+}
+.img-placeholder{
+  opacity: 0.25;
+  z-index:2
+}
+</style>>
